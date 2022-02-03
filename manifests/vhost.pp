6f2408a49148573c74767cea5295abe0328effbd
Cupa <5896309+Cupax3@users.noreply.github.com>
2022-02-03 21:24:44 +0100

    Allow to disable a virtual host
    Updates https://github.com/thechristschn/puppetlabs-apache/commit/9dc0f3914c8fab9dc01c52e23af212b4559b50b4 to current upstream master

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1218,6 +1218,9 @@
 #   If the virtual host has no assigned IP or port, the virtual host name is set to the 
 #   title of the resource.
 #
+# @param vhost_enable
+#   Specifies if the symlink to enable the virtual host is created or not.
+#
 # @param virtual_docroot
 #   Sets up a virtual host with a wildcard alias subdomain mapped to a directory with the 
 #   same name. For example, `http://example.com` would map to `/var/www/example.com`.
@@ -1818,6 +1821,7 @@ define apache::vhost (
   $override                                                                         = ['None'],
   $directoryindex                                                                   = '',
   $vhost_name                                                                       = '*',
+  Boolean $vhost_enable                                                             = true,
   $logroot                                                                          = $apache::logroot,
   Enum['directory', 'absent'] $logroot_ensure                                       = 'directory',
   $logroot_mode                                                                     = undef,
@@ -2417,7 +2421,7 @@ define apache::vhost (
   }
   # NOTE(pabelanger): This code is duplicated in ::apache::vhost::custom and
   # needs to be converted into something generic.
-  if $apache::vhost_enable_dir {
+  if $vhost_enable and $apache::vhost_enable_dir {
     $vhost_enable_dir = $apache::vhost_enable_dir
     $vhost_symlink_ensure = $ensure ? {
       'present' => link,
